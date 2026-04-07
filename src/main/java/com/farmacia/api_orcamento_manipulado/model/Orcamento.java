fb076551644bdeea6db3683a5cad1156444b86e6
lu78abranches <luisabranches.surf@hotmail.com>
2026-04-06 23:34:03 -0300

    refactor: transforma Orcamento em @Entity e utiliza Lombok

--- src/main/java/com/farmacia/api_orcamento_manipulado/model/Orcamento.java
+++ src/main/java/com/farmacia/api_orcamento_manipulado/model/Orcamento.java
@@ -1,16 +1,36 @@
 package com.farmacia.api_orcamento_manipulado.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity // Diz ao JPA que esta classe é uma tabela no banco
+@Getter // O Lombok cria todos os métodos get (inclusive o getStatus())
+@Setter // O Lombok cria todos os métodos set
+
 public class Orcamento {
 
-    /* Criamos o método, mas retornamos null ou vazio para o teste falhar no valor
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Definimos o valor padrão diretamente no atributo
+    private String status = "PENDENTE_REVISAO";
+
+    // O método getStatus() manual foi removido! O @Getter faz o trabalho.
+
+
+    /*Abaixo, codigo criado antes de criar Entidade JPA e usar o Lombok
+
+     Criamos o método, mas retornamos null ou vazio para o teste falhar no valor
     public String getStatus() {
         return null;
-    }*/
+    }
     // 1. Definimos o valor que o teste espera
     private String status = "PENDENTE_REVISAO";
 
     // 2. Retornamos esse valor no método
     public String getStatus() {
         return this.status;
-    }
+    }*/
 }
